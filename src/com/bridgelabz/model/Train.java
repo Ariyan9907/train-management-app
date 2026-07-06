cb501b2b08e00d1a7f1a5ab09a9a3ea152b12b77
ARIYAN MARUTI PUJARI <iampujari1@gmail.com>
2026-07-06 23:47:12 +0530

    [Ariyan Pujari] feat(model): add HashSet for unique bogie ID management

--- src/com/bridgelabz/model/Train.java
+++ src/com/bridgelabz/model/Train.java
@@ -1,22 +1,33 @@
 package com.bridgelabz.model;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Train {
 
     private List<String> bogies;
+    private Set<String> bogieIds;
 
-    public Train(){
-        this.bogies=new ArrayList<>();
+    public Train() {
+        bogies = new ArrayList<>();
+        bogieIds = new HashSet<>();
     }
-    public List<String> getBogies(){
+
+    public List<String> getBogies() {
         return bogies;
     }
 
-    public void setBogies(List<String> bogies){
-        this.bogies=bogies;
+    public void setBogies(List<String> bogies) {
+        this.bogies = bogies;
     }
 
+    public Set<String> getBogieIds() {
+        return bogieIds;
+    }
 
+    public void setBogieIds(Set<String> bogieIds) {
+        this.bogieIds = bogieIds;
+    }
 }
