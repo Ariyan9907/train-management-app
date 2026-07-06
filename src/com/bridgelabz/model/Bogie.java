75a7736890d2aa558c86a32f712c55e8a49111d8
ARIYAN MARUTI PUJARI <iampujari1@gmail.com>
2026-07-07 00:25:52 +0530

    [Ariyan Pujari] Updated Bogie model with bogie type

--- src/com/bridgelabz/model/Bogie.java
+++ src/com/bridgelabz/model/Bogie.java
@@ -4,11 +4,13 @@ public class Bogie {
 
     private String name;
     private int capacity;
+    private String type;
 
     // Initializes a bogie with name and seating capacity.
-    public Bogie(String name, int capacity) {
+    public Bogie(String name, int capacity, String type) {
         this.name = name;
         this.capacity = capacity;
+        this.type = type;
     }
 
     // Returns the bogie name.
@@ -22,6 +24,15 @@ public class Bogie {
     }
 
     // Returns the bogie details as a string.
+    @Override
+    public String toString() {
+        return name + " --> " + capacity + " Seats";
+    }
+
+    public String getType() {
+        return type;
+    }
+
     @Override
     public String toString() {
         return name + " --> " + capacity + " Seats";
