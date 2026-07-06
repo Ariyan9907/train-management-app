38c302fc35ba9f3f1f42fb5ade1f6a7d9f4d6deb
ARIYAN MARUTI PUJARI <iampujari1@gmail.com>
2026-07-06 23:16:33 +0530

    [Ariyan Pujari] Train application model

---
+++ src/com/bridgelabz/model/Train.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Train {
+
+    private List<String> bogies;
+
+    public Train(){
+        this.bogies=new ArrayList<>();
+    }
+    public List<String> getBogies(){
+        return bogies;
+    }
+
+    public void setBogies(List<String> bogies){
+        this.bogies=bogies;
+    }
+}
