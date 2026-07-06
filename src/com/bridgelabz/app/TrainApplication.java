f893fa1ba7dd1abe424be0c9c02f8afd3c7087b2
ARIYAN MARUTI PUJARI <iampujari1@gmail.com>
2026-07-06 23:39:21 +0530

    feat(app): update TrainApplication for passenger bogie operations

--- src/com/bridgelabz/app/TrainApplication.java
+++ src/com/bridgelabz/app/TrainApplication.java
@@ -5,9 +5,21 @@ import com.bridgelabz.service.TrainService;
 
 public class TrainApplication {
     public static void main(String[] args) {
-            TrainService trainService=new TrainService();
+        TrainService trainService = new TrainService();
+
         Train train = trainService.initializeTrain();
+
         trainService.displayTrainSummary(train);
+
+        trainService.addPassengerBogies(train);
+
+        trainService.displayBogies(train);
+
+        trainService.removePassengerBogie(train, "AC Chair");
+
+        trainService.checkPassengerBogie(train, "Sleeper");
+
+        trainService.displayBogies(train);
 
     }
 }
