261b25e6fec1a050d03c9a05c30dfc9af6b5b85a
ARIYAN MARUTI PUJARI <iampujari1@gmail.com>
2026-07-07 01:02:10 +0530

    [Ariyan Pujari] Implemented Bubble Sort for passenger bogie capacities

--- src/com/bridgelabz/service/TrainService.java
+++ src/com/bridgelabz/service/TrainService.java
@@ -367,6 +367,33 @@ public class TrainService {
 
         }
 
+
+
+    }
+    // Sorts passenger bogie capacities using Bubble Sort.
+    public void bubbleSortPassengerCapacities() {
+
+        int[] capacities = {72, 56, 24, 120, 150};
+
+        for (int i = 0; i < capacities.length - 1; i++) {
+
+            for (int j = 0; j < capacities.length - i - 1; j++) {
+
+                if (capacities[j] > capacities[j + 1]) {
+
+                    int temp = capacities[j];
+                    capacities[j] = capacities[j + 1];
+                    capacities[j + 1] = temp;
+
+                }
+            }
+        }
+
+        System.out.println("\n========= Bubble Sorted Capacities =========");
+
+        for (int capacity : capacities) {
+            System.out.println(capacity);
+        }
     }
 
 
