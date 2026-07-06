ca1fcb431e97e872deae21a8ebff3733bb9347a2
ARIYAN MARUTI PUJARI <iampujari1@gmail.com>
2026-07-06 23:39:59 +0530

    feat(service): implement passenger bogie management using ArrayList

--- src/com/bridgelabz/service/TrainService.java
+++ src/com/bridgelabz/service/TrainService.java
@@ -26,4 +26,40 @@ public class TrainService {
         System.out.println("Current Bogie Count : " + train.getBogies().size());
     }
 
+    public void addPassengerBogies(Train train) {
+
+        train.getBogies().add("Sleeper");
+        train.getBogies().add("AC Chair");
+        train.getBogies().add("First Class");
+
+        System.out.println("Passenger bogies added successfully.");
+    }
+
+    public void displayBogies(Train train) {
+
+        System.out.println("Current Passenger Bogies:");
+
+        for (String bogie : train.getBogies()) {
+            System.out.println(bogie);
+        }
+    }
+
+    public void removePassengerBogie(Train train, String bogieName) {
+
+        if (train.getBogies().remove(bogieName)) {
+            System.out.println("Removed Bogie : " + bogieName);
+        } else {
+            System.out.println("Bogie not found : " + bogieName);
+        }
+    }
+
+    public void checkPassengerBogie(Train train, String bogieName) {
+
+        if (train.getBogies().contains(bogieName)) {
+            System.out.println("\n" + bogieName + " is available.");
+        } else {
+            System.out.println("\n" + bogieName + " is not available.");
+        }
+    }
+
 }
